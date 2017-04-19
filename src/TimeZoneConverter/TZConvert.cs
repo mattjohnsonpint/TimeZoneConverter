d360364b83f70584d7020ee255ce543b5c47ec21
Matt Johnson <mj1856@hotmail.com>
2017-04-19 15:21:53 -0700

    Add XML Docs

--- src/TimeZoneConverter/TZConvert.cs
+++ src/TimeZoneConverter/TZConvert.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace TimeZoneConverter
 {
+    /// <summary>
+    /// Converts time zone identifiers from various sources.
+    /// </summary>
     public static class TZConvert
     {
         private static readonly IDictionary<string, string> IanaMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
@@ -13,6 +16,12 @@ namespace TimeZoneConverter
             DataLoader.Populate(IanaMap, WindowsMap);
         }
 
+        /// <summary>
+        /// Converts an IANA time zone name to the equivalent Windows time zone ID.
+        /// </summary>
+        /// <param name="ianaTimeZoneName">The IANA time zone name to convert.</param>
+        /// <returns>A Windows time zone name.</returns>
+        /// <exception cref="InvalidTimeZoneException">Thrown if the input string was not recognized or has no equivalent Windows zone.</exception>
         public static string IanaToWindows(string ianaTimeZoneName)
         {
             string windowsTimeZoneId;
@@ -22,6 +31,16 @@ namespace TimeZoneConverter
             throw new InvalidTimeZoneException($"\"{ianaTimeZoneName}\" was not recognized as a valid IANA time zone name, or has no equivalant Windows time zone.");
         }
 
+        /// <summary>
+        /// Converts a Windows time zone ID to an equivalent IANA time zone name.
+        /// </summary>
+        /// <param name="windowsTimeZoneId">The Windows time zone ID to convert.</param>
+        /// <param name="territoryCode">
+        /// An optional two-letter ISO Country/Region code, used to get a a specific mapping.
+        /// Defaults to "001" if not specified, which means to get the "golden zone" - the one that is most prevalent.
+        /// </param>
+        /// <returns>An IANA time zone ID.</returns>
+        /// <exception cref="InvalidTimeZoneException">Thrown if the input string was not recognized or has no equivalent IANA zone.</exception>
         public static string WindowsToIana(string windowsTimeZoneId, string territoryCode = "001")
         {
             var key = $"{territoryCode}|{windowsTimeZoneId}";
