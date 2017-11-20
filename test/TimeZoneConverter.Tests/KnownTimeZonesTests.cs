137566c8fc6b4f9378bbac4349c03b05b832c6cb
Matt Johnson <mj1856@hotmail.com>
2017-11-19 18:00:31 -0800

    Add some tests

---
+++ test/TimeZoneConverter.Tests/KnownTimeZonesTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace TimeZoneConverter.Tests
+{
+    public class KnownTimeZonesTests
+    {
+        [Fact]
+        public void Can_Get_Known_IANA_TimeZones()
+        {
+            Assert.NotEmpty(TZConvert.KnownIanaTimeZoneNames);
+        }
+
+        [Fact]
+        public void Can_Get_Known_Windows_TimeZones()
+        {
+            Assert.NotEmpty(TZConvert.KnownWindowsTimeZoneIds);
+        }
+
+        [Fact]
+        public void Can_Get_Known_Rails_TimeZones()
+        {
+            Assert.NotEmpty(TZConvert.KnownRailsTimeZoneNames);
+        }
+    }
+}
