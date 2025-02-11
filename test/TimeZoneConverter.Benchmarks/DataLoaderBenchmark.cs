e3ab4d1b003a496a4d20c28cb01fde17da87114c
Marko Lahma <marko.lahma@gmail.com>
2025-02-11 20:04:45 +0200

    Add BenchmarkDotNet benchmark project and DataLoader benchmark (#159)
    Co-authored-by: Matt Johnson-Pint <mattjohnsonpint@gmail.com>

---
+++ test/TimeZoneConverter.Benchmarks/DataLoaderBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace TimeZoneConverter.Benchmarks;
+
+[MemoryDiagnoser]
+public class DataLoaderBenchmark
+{
+    private static readonly Dictionary<string, string> IanaMap = new();
+    private static readonly Dictionary<string, string> WindowsMap = new();
+    private static readonly Dictionary<string, string> RailsMap = new();
+    private static readonly Dictionary<string, IList<string>> InverseRailsMap = new();
+    private static readonly Dictionary<string, string> Links = new();
+    private static readonly Dictionary<string, IList<string>> IanaTerritoryZones = new();
+
+    [IterationSetup]
+    public void IterationSetup()
+    {
+        IanaMap.Clear();
+        WindowsMap.Clear();
+        RailsMap.Clear();
+        InverseRailsMap.Clear();
+        Links.Clear();
+        IanaTerritoryZones.Clear();
+    }
+
+    [Benchmark]
+    public void Load()
+    {
+        DataLoader.Populate(IanaMap, WindowsMap, RailsMap, InverseRailsMap, Links, IanaTerritoryZones);
+    }
+}
