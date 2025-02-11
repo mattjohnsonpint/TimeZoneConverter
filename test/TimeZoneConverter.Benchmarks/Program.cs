61e82dcdf455b8648d41904f0b9b75b92381792d
Marko Lahma <marko.lahma@gmail.com>
2025-02-11 09:11:11 +0200

    Add BenchmarkDotNet benchmark project and DataLoader benchmark

---
+++ test/TimeZoneConverter.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace TimeZoneConverter.Benchmarks;
+
+public static class Program
+{
+    public static void Main(string[]? args)
+    {
+        BenchmarkSwitcher.FromAssembly(typeof(DataLoaderBenchmark).Assembly).Run(args);
+    }
+}
